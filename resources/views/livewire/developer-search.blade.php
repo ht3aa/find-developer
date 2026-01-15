4f36fe5276d4b8a0007ab053480dfef213c83a2b
ht3aa <ht3aa2001@gmail.com>
2026-01-15 15:14:38 +0300

    update

--- resources/views/livewire/developer-search.blade.php
+++ resources/views/livewire/developer-search.blade.php
@@ -130,7 +130,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
                                 @if($developer->expected_salary_from && $developer->expected_salary_to)
-                                    {{ number_format($developer->expected_salary_from) }} - {{ number_format($developer->expected_salary_to) }} IQD/year
+                                    {{ number_format($developer->expected_salary_from) }} - {{ number_format($developer->expected_salary_to) }} IQD/month
                                 @elseif($developer->expected_salary_from)
                                     From {{ number_format($developer->expected_salary_from) }} IQD/year
                                 @else
