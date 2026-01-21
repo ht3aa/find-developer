d9ed38b6d1d2e0f9605518cd3b163a555b8429de
ht3aa <ht3aa2001@gmail.com>
2026-01-22 00:49:20 +0300

    update

--- resources/views/charts.blade.php
+++ resources/views/charts.blade.php
@@ -5,6 +5,10 @@
 @section('seo_description', 'View analytics and statistics about developers, jobs, and platform data. Visual insights into developer status, subscription plans, locations, and experience levels.')
 @section('seo_keywords', 'developer analytics, developer statistics, developer insights, job statistics, developer charts')
 
+@push('styles')
+<link href="{{ asset('css/charts.css') }}" rel="stylesheet">
+@endpush
+
 @section('content')
     @livewire('charts')
 @endsection
