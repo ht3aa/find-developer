b4e7a4bba8efd6d2f29608199958d61624190128
ht3aa <ht3aa2001@gmail.com>
2026-02-17 19:37:04 +0300

    update

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,18 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\DeveloperBlog;
+use App\Models\Scopes\DeveloperScope;
 
 class BlogController extends Controller
 {
     public function show($slug)
     {
         $blog = DeveloperBlog::with(['developer', 'developer.jobTitle'])
+            ->withoutGlobalScopes([DeveloperScope::class])
             ->published()
             ->where('slug', $slug)
             ->firstOrFail();
 
         // Get related blogs (same developer, excluding current blog)
         $relatedBlogs = DeveloperBlog::with(['developer', 'developer.jobTitle'])
+            ->withoutGlobalScopes([DeveloperScope::class])
             ->published()
             ->where('developer_id', $blog->developer_id)
             ->where('id', '!=', $blog->id)
