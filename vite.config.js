e82e48cae438c6fcbd5ed9b85cb06e3ddb46763f
ht3aa <ht3aa2001@gmail.com>
2026-02-21 16:37:10 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/companies.css',
                 'resources/js/app.js',
                 'resources/css/filament/admin/theme.css',
                 'resources/css/base.css',
