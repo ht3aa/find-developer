94340a1e7ecc5e9e0ddb2fca444b119b1b408168
ht3aa <ht3aa2001@gmail.com>
2026-02-26 06:33:54 +0300

    update

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -33,7 +33,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <Head title="Welcome">
+    <Head title="Find Developer" description="Find the right developer for your project">
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
