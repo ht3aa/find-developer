b589821ab72ae845959521e24e29752868c5ea37
ht3aa <ht3aa2001@gmail.com>
2026-02-27 16:15:52 +0300

    update

--- public/build/assets/chevron-left-1U9LTvTf.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./createLucideIcon-C286zt9-.js";const t=e("ChevronLeftIcon",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{t as C};
---
+++ public/build/assets/chevron-left-DQSPt06M.js
@@ -0,0 +1,1 @@
+import{c as e}from"./createLucideIcon-C_9afv_H.js";const t=e("ChevronLeftIcon",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{t as C};
