d3187720b002ed4b547c6fe7bfff7c77f722a5eb
ht3aa <ht3aa2001@gmail.com>
2026-03-04 16:15:47 +0300

    update

---
+++ public/build/assets/Spinner.vue_vue_type_script_setup_true_lang-B0w2lJEj.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-BFp1nIu6.js";import{L as r}from"./loader-circle-ByLF3ZAe.js";import{d as n,k as c,u as s,n as t,o as l}from"./app-B2b-yuK5.js";const f=n({__name:"Spinner",props:{class:{}},setup(a){const e=a;return(i,p)=>(l(),c(s(r),{role:"status","aria-label":"Loading",class:t(s(o)("size-4 animate-spin",e.class))},null,8,["class"]))}});export{f as _};
