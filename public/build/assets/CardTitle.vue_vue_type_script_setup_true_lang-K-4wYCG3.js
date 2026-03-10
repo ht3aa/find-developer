91a3f7b02a005349510aed694684a4fba1250e21
ht3aa <ht3aa2001@gmail.com>
2026-03-10 14:54:48 +0300

    update

---
+++ public/build/assets/CardTitle.vue_vue_type_script_setup_true_lang-K-4wYCG3.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-CAnCkg1b.js";import{d as n,c as t,a0 as r,n as l,u as c,o as m}from"./app-DMxvQwEV.js";const f=n({__name:"CardTitle",props:{class:{}},setup(s){const a=s;return(e,p)=>(m(),t("h3",{"data-slot":"card-title",class:l(c(o)("leading-none font-semibold",a.class))},[r(e.$slots,"default")],2))}});export{f as _};
