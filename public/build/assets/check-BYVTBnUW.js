6d5202f4ca9d600bb8de1ddc2a13a548dccff086
ht3aa <ht3aa2001@gmail.com>
2026-03-13 17:17:40 +0300

    update

--- public/build/assets/check-BYVTBnUW.js
+++
@@ -1,1 +0,0 @@
-import{c}from"./createLucideIcon-lx3jeZo7.js";const o=c("CheckIcon",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]);export{o as C};
---
+++ public/build/assets/chevron-left-zWE3RWts.js
@@ -0,0 +1,1 @@
+import{c as e}from"./createLucideIcon-C8A9LoKU.js";const t=e("ChevronLeftIcon",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{t as C};
