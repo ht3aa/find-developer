6843d48fa66a398d7d57b2388613f255de67eb0f
ht3aa <ht3aa2001@gmail.com>
2026-03-31 00:15:20 +0300

    update

--- tests/Feature/ShopTest.php
+++ tests/Feature/ShopTest.php
@@ -9,3 +9,9 @@ test('shop page renders for guests', function () {
     $response->assertInertia(fn (Assert $page) => $page
         ->component('Shop'));
 });
+
+test('shop product cards document free delivery in the page source', function () {
+    $shop = file_get_contents(resource_path('js/pages/Shop.vue'));
+
+    expect($shop)->toContain('Free delivery.');
+});
